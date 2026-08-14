ed3adbd8f9588e7a3f84f7b86631628115d01caa
TTMSADAO <ping.juppy@gmail.com>
2026-08-14 10:05:31 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // ⚠️ กฎเหล็ก: อัปเดตเวอร์ชัน CACHE_NAME ทุกครั้งที่มีการแก้โค้ดใน index.html
-const CACHE_NAME = 'sadao-queue-v3.10';
+const CACHE_NAME = 'sadao-queue-v3.11';
 
 const ASSETS_TO_CACHE = [
   './',
